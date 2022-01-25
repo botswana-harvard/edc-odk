01f201e66340ad2a6c334799d6fe0034a4466539
ame diphoko <diphokoame@gmail.com>
2022-01-25 16:38:13 +0200

    remove custom dashboard from odk

--- edc_odk/navbars.py
+++ edc_odk/navbars.py
@@ -1,15 +1,7 @@
-from django.conf import settings
 from edc_navbar import NavbarItem, site_navbars, Navbar
 
 
 odk_dashboard = Navbar(name='edc_odk_forms')
-
-odk_dashboard.append_item(
-    NavbarItem(
-        name='edc_odk_forms',
-        label='ODK Archive',
-        fa_icon='fa fa-file-image',
-        url_name=settings.DASHBOARD_URL_NAMES.get('odk_listboard_url')))
 
 odk_dashboard.append_item(
     NavbarItem(name='edc_odk_usage',
